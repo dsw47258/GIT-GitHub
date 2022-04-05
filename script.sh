c0d1155fa781a6c39c1f86a4a5b1c77369ae9b1d
Karina <dsw47258@student.dsw.edu.pl>
2022-04-05 20:38:29 +0200

    pierwszy commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+git commit;
+git branch;
+git checkout;
+git checkout -b;
+git merge;
+git rebase;
+git reset;
+git revert;
+git cherry-pick;
+git tag;
+git rebase -i;
+git branch -f;
+git branch -u;
+git clone;
+git fetch;
+git pull;
+git pull --rebase;
+git push;
+git fakeTeamwork;
+
